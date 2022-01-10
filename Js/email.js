8fa792c22d27b6e65d4c4a69e848f1f2ff66e429
lindabettini <linda.bettini@hotmail.it>
2022-01-10 14:35:22 +0100

    add docs + addresses in email.js

---
+++ Js/email.js
@@ -0,0 +1,13 @@
+console.log("OK")
+
+/* 
+# Mail
+1-Crea un array di email;
+2-Chiedi all’utente la sua email.
+3-controlla che sia nella lista di chi può accedere, 
+4-stampa un messaggio appropriato in console sull’esito del controllo.
+*/
+
+const addresses = [`carolina@gmail.com`, `luca.gmail.com`, `pietro@hotmail.com`, `sara@gmail.com`]
+
+console.table(addresses)
