41d3b03da241e55649d0c0580093e643de9ee92a
lindabettini <linda.bettini@hotmail.it>
2022-01-10 16:53:49 +0100

    Add all main steps

--- dadi/js/dadi.js
+++ dadi/js/dadi.js
@@ -7,4 +7,22 @@
 4-Stampare in console i tiri di dado e il risultato. 
 */
 
-console.log("ok")
+//ç 1-Generare un numero random da 1 a 6 per il giocatore.
+const gamerNumber = Math.random();
+console.log(Math.floor(gamerNumber * 6) + 1);
+
+//ç 2-Generare un numero random da 1 a 6 per il computer.
+const computerNumber = Math.random();
+console.log(Math.floor(computerNumber * 6) + 1);
+
+//ç 3-Stabilire il vincitore, in base a chi fa il punteggio più alto.
+let winner = false;
+
+if (gamerNumber > computerNumber){
+    winner = true;
+    console.log(`Hai vinto!`);
+    else {
+        console.log(`Hai perso!`);
+    }
+}
+
